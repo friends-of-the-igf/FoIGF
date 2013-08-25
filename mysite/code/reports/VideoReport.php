72072c402e7a1087c89ddd62ed58637241fcd727
Ben Manu <ben@stripetheweb.com>
2013-08-26 11:07:04 +1200

    Adding missing videos report

---
+++ mysite/code/reports/VideoReport.php
@@ -0,0 +1,39 @@
+<?php
+
+class VideoReport extends SS_Report {
+
+	function title() {
+		return "Missing Videos Report";
+	}
+	
+	function sourceRecords($params, $sort, $limit) {
+		$sessions = MeetingSession::get()->sort($sort);
+		$list = new ArrayList();
+		foreach($sessions as $session) {
+			if($session->Videos()->Count() == 0) {
+				$list->push($session);
+			}
+		}
+		return $list;
+	}
+
+	function columns() {
+		$fields = array(
+			"ID" => array(
+				"title" => "ID"
+			),
+			"Meeting.Title" => array(
+				"Title" => "Meeting"
+			),
+			"Title" => array(
+				"title" => "Title"
+			),
+			"Date" => array(
+				"Title" => "Date"
+			)
+		);
+		
+		return $fields;
+	}
+
+}
