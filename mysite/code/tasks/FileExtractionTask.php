4739ce90a026293f96724c2d1e8f93852b0a7bd3
Ben Manu <ben@stripetheweb.com>
2013-10-11 11:17:28 +1300

    IGF-46 adding text extraction task for existing files

---
+++ mysite/code/tasks/FileExtractionTask.php
@@ -0,0 +1,23 @@
+<?php
+
+class FileExtractionTask extends BuildTask{
+	
+	protected $title = "Extract File Content";
+	
+	protected $description = "Extracts all file content using Solrs text extraction";
+	
+	function run($request){
+		$files = File::get();
+
+		foreach($files as $file) {
+			if(!$file->FileContentCache) {
+				$file->extractFileAsText(true);
+			}
+			echo '.';
+		}
+		echo 'done';
+	}
+
+
+	
+}
