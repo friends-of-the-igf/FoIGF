98dd4f9fb7dc24deb6c1d4912f82bc8247f989a8
Dylan Sweetensen <dylan@stripetheweb.com>
2013-10-11 10:18:29 +1300

    Implemented

--- mysite/code/controllers/SessionController.php
+++ mysite/code/controllers/SessionController.php
@@ -8,13 +8,17 @@ class SessionController extends Page_Controller {
 
 	public static $allowed_actions = array(
         'TagForm',
-        'saveTags'
+        'saveTags',
+        'getTags'
 	);
 
 	protected $meetingsession = null;
 
 	public function init() {
 		parent::init();
+
+		Requirements::javascript('themes/igf/javascript/sessioncontroller.js');
+		Requirements::javascript('themes/igf/thirdparty/bootstrap-typeahead.js');
 
 		$id = (int)$this->request->param('ID');
 		if($meetingsession = MeetingSession::get()->ByID($id)) {
@@ -38,7 +42,7 @@ class SessionController extends Page_Controller {
 			Session::set('CurrentSession', $meetingsession);
 			$this->meetingsession = $meetingsession;
 		} else {
-			if($this->request->param('Action') == 'CustomSearchForm' || $this->request->param('Action') == 'TagForm'){
+			if($this->request->param('Action') == 'CustomSearchForm' || $this->request->param('Action') == 'TagForm' || $this->request->param('Action') == 'getTags'){
 				return;
 			}else{
 				return $this->httpError(404);
@@ -58,36 +62,79 @@ class SessionController extends Page_Controller {
 		return 'SessionController';
 	}
 
-	// public function TagForm(){
-	// 	$fields = new FieldList();
+	public function TagForm(){
+		$fields = new FieldList();
 
-	// 	$fields->push($t = new TextField('Tags', 'Add Tags'));
-	// 	$t->setAttribute('placeholder', 'Enter tags separated by commas');
+		$fields->push($t = new TextField('Tags', 'Add Tags'));
+		$t->setAttribute('placeholder', 'Enter tags separated by commas');
+		$t->setAttribute('class', 'typeahead');
+		$t->setAttribute('data-provide', 'typeahead');
+		$t->setAttribute('autocomplete', 'off');
 
-	// 	$actions = new FieldList($b = new FormAction('saveTags', 'Save'));
-	// 	$b->addExtraClass('btn');
-	// 	$b->addExtraClass('btn-primary');
+		$actions = new FieldList($b = new FormAction('saveTags', 'Save'));
+		$b->addExtraClass('btn');
+		$b->addExtraClass('btn-primary');
 
-	// 	$form = new Form($this, 'TagForm', $fields, $actions);
+		$form = new Form($this, 'TagForm', $fields, $actions);
+		$form->setAttribute('data-url', $this->Link());
 
-	// 	return $form;
-	// }
+		return $form;
+	}
 
-	// public function saveTags($data, $form){
-	// 	$meetingsession = Session::get('CurrentSession');
+	public function saveTags($data, $form){
+		$meetingsession = Session::get('CurrentSession');
+		if($data['Tags'] != null && isset($data['Tags'])){
+			//make an array of the tags currently attached to sesseion		
+			$oldTagList = array();	
+			$oldTags = preg_split("*,*", trim($meetingsession->Tags));
+			foreach($oldTags as $tag) {
+				if($tag != "") {
+					$tag = strtolower($tag);
+					$oldTagList[$tag] = $tag;
+				}
+			}
+
+			$newTagList = array();	
+			$newTags = preg_split("*,*", trim($data['Tags']));
+			foreach($newTags as $tag) {
+				$tag = trim($tag);
+				if($tag != "") {
+					$tag = strtolower($tag);
+					$newTagList[$tag] = $tag;
+				}
+			}
+
+			$tagsToAdd = array_diff($newTagList, $oldTagList);
 		
-	// 	if($data['Tags'] != null && isset($data['Tags'])){
-	// 		if($meetingsession->Tags != null){
-				
-	// 			$meetingsession->Tags .= ',' . $data['Tags'];
-	// 		} else {
-	// 			$meetingsession->Tags = $data['Tags'];
-	// 		}
-	// 		$meetingsession->write();
-	// 	}
-	// 	return $this->redirectBack();
+			if(!empty($tagsToAdd)){
+				foreach($tagsToAdd as $tag){
+					
+					if($meetingsession->Tags != null){
+						$meetingsession->Tags .= ','.$tag;
+					} else {
+						$meetingsession->Tags = $tag;
+					}
+					$meetingsession->write();
+				}
+			}
+		}
+		return $this->redirectBack();
+	}
 
-	// }
+	public function getTags() {
+		$sessions = MeetingSession::get();
+		$list = array();	
+		foreach($sessions as $session) {
+			$tags = preg_split("*,*", trim($session->Tags));
+			foreach($tags as $tag) {
+				if($tag != "") {
+					$tag = strtolower($tag);
+					$list[$tag] = $tag;
+				}
+			}
+		}
+		return json_encode($list);
+	}
 
 
 }
