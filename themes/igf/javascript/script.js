8aa49d5fdece90d135fea2a74a8eb313954b4949
Dylan Sweetensen <dylan@stripetheweb.com>
2014-05-01 13:05:44 +1200

    IGF-63 more testing

--- themes/igf/javascript/script.js
+++ themes/igf/javascript/script.js
@@ -3,8 +3,10 @@ jQuery.noConflict();
 (function($) {
 $(document).ready(function() {
 
-	$('#Form_SearchForm').on('click', function(){
-		ga('send', 'event', 'GeneralSearch', 'Submit', $('#Form_SearchForm_Search').val());
+	$('#Form_SearchForm').on('click', function(e){
+		e.preventDefault();
+		ga('send', 'event', 'GeneralSearch', 'Submit', 'Search Term: ' + $('#Form_SearchForm_Search').val());
+		ga('send', 'event', 'OtherCategory', 'OtherAction', 'HAHAHAH');
 	});
 
 	$('#Form_SearchForm').submit(function(e){
