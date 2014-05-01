9b5dbc540d00c79eafb3bc262163333aaaa4c65d
Dylan Sweetensen <dylan@stripetheweb.com>
2014-05-01 12:48:19 +1200

    IGF-63 testing analytics

--- themes/igf/javascript/script.js
+++ themes/igf/javascript/script.js
@@ -4,6 +4,7 @@ jQuery.noConflict();
 $(document).ready(function() {
 
 	$('#Form_SearchForm').submit(function(e){
+		ga('send', 'event', 'button', 'click', 'QR_Win_A_Grand', 'Clicked_through_to_register');
 		if($.trim($('#Form_SearchForm_Search').val() ) == ''){
 			e.preventDefault();
 		}
