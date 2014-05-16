d7d7b4f645bae322e8147617c7727c2f5f6b3765
Dylan Sweetensen <dylan@stripetheweb.com>
2014-05-16 17:02:46 +1200

    Refind function and started creating interface

--- mysite/code/pages/OpenCalaisPage.php
+++ mysite/code/pages/OpenCalaisPage.php
@@ -8,21 +8,90 @@ class OpenCalaisPage extends Page{
 class OpenCalaisPage_Controller extends Page_Controller{
 
 	public static $allowed_actions = array(
-		'test'
+		'test',
+		'OpenCalaisForm'
 	);
+
+	public function OpenCalaisForm(){
+
+		$sessions = MeetingSession::get()->map();
+
+		$fields = new FieldList(array(
+			new DropdownField('Session', 'Choose a session:', $sessions),
+			new CheckboxSetField('ContentSelection', 'Select what areas of content you would like to process:', array('Transcripts' => 'Transcript', 'Agenda' =>  'Agenda', 'Proposal' =>  'Proposal', 'Report' =>  'Report'))
+			)
+		);
+
+		$actions = new FieldList(new FormAction('processSession', 'Extract Entities'));
+
+		return new Form($this, 'OpenCalaisForm', $fields, $actions);
+	}
+
+	public function processSession($data, $form){
+		$session =  MeetingSession::get()->byID($data['Session']);
+		$returnData = array();
+		$ocs = new OpenCalaisService();
+		foreach($data['ContentSelection'] as $area){
+			if($area == 'Agenda'){
+				$content = $session->Content;
+			} else if($area == 'Transcripts') {
+				if($session->Transcripts()->filter('TranscriptType', 'Text')->Count() != 0){
+					$content = $session->Transcripts()->filter('TranscriptType', 'Text')->First()->Content;
+				} else {
+					$returnData[$area] = 'Transcript was not available in a format that can be processed or does not exist. You can only process text based Transcripts.';
+					break;
+				}
+			} else if($area == 'Proposal'){
+				if($session->ProposalType == 'Text' && strlen($session->ProposalContent) > 0){
+					$content = $session->ProposalContent;
+				} else {
+					$returnData[$area] = 'Proposal was not available in a format that can be processed or does not exist. You can only process text based Proposals.';
+					break;
+				}
+			} else {
+				if($session->ReportType == 'Text' && strlen($session->ReportContent) > 0){
+					$content = $session->ReportContent;
+				} else {
+					$returnData[$area] = 'Report was not available in a format that can be processed or does not exist. You can only process text based Reports.';
+					break;
+				}
+			}
+
+			//TO DO: Move this process to part of the service class
+			if(strlen($content) > 1000){
+				$length = strlen($content);
+				$chunks = floor(strlen($content)/1000) + 1;
+				$start = 0;
+				$end = 1000;
+				$nextPeriod = strpos($content, '.', $end)+1 - $start;
+				$contentChunks = array();
+				for($i = 0; $i < $chunks; $i++){				
+					$contentChunks[] = substr($content, $start, $nextPeriod);
+					$start = $start + $nextPeriod;
+					$end = $end + 1000;
+					if($end > $length){
+						$nextPeriod = $length;
+					} else{
+						$nextPeriod = strpos($content, '.', $end)+1-$start;
+					}
+				}
+			}
+			foreach($contentChunks as $chunk){
+				$ocs->setContent($chunk);
+				Debug::dump($ocs->processContent());
+			}
+			
+		}
+
+		// return Debug::dump($returnData);
+
+	}
 
 
 	public function test(){
 		$ocs = new OpenCalaisService();
-		$ocs->setContent("Matt Gunn");
+		$ocs->setContent("April 7 (Bloomberg) . Yahoo! Inc., the Internet company that snubbed a $44.6 billion takeover bid from Microsoft Corp., may drop in Nasdaq trading after the software maker threatened to cut its bid if directors fail to give in soon. If Yahoo.s directors refuse to negotiate a deal within three weeks, Microsoft plans to nominate a board slate and take its case to investors, Chief Executive Officer Steve Ballmer said April 5 in a statement. He suggested the deal.s value might decline if Microsoft has to take those steps. The ultimatum may send Yahoo Chief Executive Officer Jerry Yang scrambling to find an appealing alternative for investors to avoid succumbing to Microsoft, whose bid was a 62 percent premium to Yahoo.s stock price at the time. The deadline shows Microsoft is in a hurry to take on Google Inc., which dominates in Internet search, said analysts including Canaccord Adams.s Colin Gillis.");
 		$result = $ocs->processContent();
-
-
-		$extractor = new OpenCalaisXMLParser($result);
-
-	
-		return Debug::dump($extractor->getCategories());
-		
-		
+		return Debug::dump($result);
 	}
 }
--- opencalais/code/OpenCalaisService.php
+++ opencalais/code/OpenCalaisService.php
@@ -6,6 +6,7 @@ class OpenCalaisService extends RestfulService{
 	*
 	*/
 	protected $content;
+
 	/**
 	*
 	*/
@@ -19,7 +20,7 @@ class OpenCalaisService extends RestfulService{
 	*
 	*/
 	public function processContent(){
-
+		 
 		//Get and set the API key from settings. Throw error if it hasn't been entered.
 		$key = SiteConfig::current_site_config()->OpenCalaisAPIKey;
 		if($key == null){
@@ -28,15 +29,14 @@ class OpenCalaisService extends RestfulService{
 		$params['licenseID'] = $key;
 
 		//Set paramsXML
-		$pXML = "<c:params xmlns:c=\"http://s.opencalais.com/1/pred/\" " . 
-			"xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"> " .
-			"<c:processingDirectives c:contentType=\"text/html\" " .
-			"c:outputFormat=\"text/simple\"".
-			" c:omitOutputtingOriginalText=\"TRUE\"></c:processingDirectives> " .
-			"<c:userDirectives c:allowDistribution=\"true\" " .
-			"c:allowSearch=\"true\" c:externalID=\" \" " .
-			"></c:userDirectives> " .
-			"</c:params>";
+		$pXML = 
+		'<c:params xmlns:c="http://s.opencalais.com/1/pred/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+			<c:processingDirectives c:contentType="text/html" c:enableMetadataType="GenericRelations,SocialTags" c:outputFormat="text/simple" >
+			</c:processingDirectives>
+			<c:userDirectives c:allowDistribution="false" c:allowSearch="false" >
+			</c:userDirectives>
+			<c:externalMetadata></c:externalMetadata>
+		</c:params>';
 
 		$params['paramsXML'] = $pXML;
 
@@ -45,11 +45,88 @@ class OpenCalaisService extends RestfulService{
 
 		$this->setQueryString($params);
 
-		return $this->request();
+		$response = $this->request();
+		$xml = $response->SimpleXML()->CalaisSimpleOutputFormat;
+		Debug::dump($response);
+		return $this->getEntities($xml);
 	}
 
 	public function setContent(String $content){
 		$this->content = $content;
 	}
 
+
+	/**
+	*
+	*/
+	public function getEntities($xml){
+		//Entities
+		$types = array();
+		foreach($xml->children() as $child){
+			$types[$child->getName()] = $child->getName();
+		}
+		$typeCount = array();
+		foreach($types as $type){
+			$typeCount[$type] = count($xml->$type);
+		}
+		foreach($types as $type){
+			$entities = array();
+			$count = $typeCount[$type];
+			for($i = 0; $i < $count; $i++){
+				$entityList = $xml->$type;
+				$entity = $entityList[$i];
+				$attributes = $entity->attributes();
+				$entityData = array(
+					'Type' => $entity->getName(),
+					'Value' => (string)$entity,
+					'Normalized' => str_replace(array('normalized', '=', '"'), '', $attributes['normalized']),
+					'Relevance' => floatval(str_replace(array('relevance', '=', '"'), '', $attributes['relevance'])),
+					'Count' => (int)(str_replace(array('count', '=', '"'), '', $attributes['count'])),
+				);
+				$entities[$entityData['Value']] = $entityData;
+			}
+			$types[$type] = $entities;
+		}
+
+		//Topic
+		$topics = $xml->Topics;
+		if($topics){
+			$topicArray = array();
+			foreach($topics->Topic as $topic){
+				$score = $topic->Attributes();
+				$topicArray[] = array(
+					'Topic' => str_replace('_', ' and ', $topic->asXML()),
+					'Value' => (string)$topic,
+					'Score' => floatval(str_replace(array('Score', '=', '"'), '', $score['Score']->asXML()))
+					);
+			}
+		
+		} else {
+			$topicArray = 'No topics were able to be extracted from the content';
+		}
+		$types['Topics'] = $topicArray;
+
+		//Social Tags
+		$tags = $xml->SocialTags;
+		if($tags){
+			$tagsArray = array();
+			foreach($tags->SocialTag as $tag){
+				$importance = $tag->Attributes();
+				$tagsArray[] = array(
+					'Tag' => (string)$tag,
+					'Importance' => floatval(str_replace(array('importance', '=', '"'), '', $importance['importance']->asXML()))
+					);
+			}
+		} else {
+			$tagsArray = 'No social tags were able to be extracted from the content';
+		}
+		$types['SocialTags'] = $tagsArray;
+
+
+		return $types;
+	}
+
+	public function prepareContent(){
+		
+	}
 }
